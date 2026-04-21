4306d8575af275fdd8815063526f7f14973dd962
irinakiseleva0 <irina.kiseleva@epita.fr>
2026-04-21 14:20:13 +0200

    Add authentication middleware for protected routes

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,21 @@
+function authCheck(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  if (!authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  next();
+}
+
+module.exports = authCheck;
