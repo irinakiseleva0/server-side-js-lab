dbf4ef5f5b188ab6ebcb28427d47673f67a601ec
irinakiseleva0 <irina.kiseleva@epita.fr>
2026-04-21 14:18:58 +0200

    Create protected Course routes

---
+++ routes/courseRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const courseController = require("../controllers/courseController");
+const authCheck = require("../middleware/auth-middleware");
+
+router.get("/", authCheck, courseController.getAll);
+router.get("/:id", authCheck, courseController.getById);
+router.post("/", authCheck, courseController.create);
+router.put("/:id", authCheck, courseController.update);
+router.delete("/:id", authCheck, courseController.remove);
+
+module.exports = router;
